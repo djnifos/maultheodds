97de2fe579e1304cc4b276150415c73df91fa4c9
djnifos <djnifos@gmail.com>
2024-04-11 19:33:58 -0500

    Add files via upload

---
+++ kubejs/server_scripts/mods/roguelite.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(event => {
+    event.shaped('roguelitestorage:safe', [
+        'AAA', 
+        'ABA', 
+        'AAA'  
+        ], {
+        A: 'xycraft_world:kivi', 
+        B: 'minecraft:quartz' 
+        }
+    );    
+});
---
+++ kubejs/server_scripts/mods/zombieExtreme.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('item', event => {
+    event.add('forge:ingots/tin', 'zombie_extreme:ignot_tin')
+    event.add('forge:ingots/aluminum', 'zombie_extreme:ignot_aluminum')
+    event.add('forge:ingots/bronze', 'zombie_extreme:ignot_bronze')
+    event.add('forge:ingots/steel', 'zombie_extreme:ignotsteel')
+    event.add('forge:ingots/lead', 'zombie_extreme:ignotlead')
+})
